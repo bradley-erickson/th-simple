3db386247a8a37df1e22055b71354f5f22e9ba53
Bradley Erickson <bbwe24@gmail.com>
2024-03-29 17:12:25 -0400

    added loading

---
+++ src/components/loading.py
@@ -0,0 +1,9 @@
+loading_messages = [
+    "Fetching the requested data... Please hold on!",
+    "Running analysis, this might take a moment. Your patience is appreciated!",
+    "Gathering insights... Thank you for your patience!",
+    "Crunching numbers and analyzing data. Hang tight!",
+    "We're working hard to get you the information. Please stay with us!",
+    "Loading your personalized insights. Just a bit longer!",
+    "Thank you for waiting. We're making sure everything is perfect!",
+]
