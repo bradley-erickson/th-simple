04c8ae7662602ae718bff2ecfa8a8e6871c29bc4
Bradley Erickson <bbwe24@gmail.com>
2024-04-09 08:13:31 -0400

    update redis start

--- src/start-redis.sh
+++ src/start-redis.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 less /etc/sysctl.conf
-sysctl -w net.core.somaxconn=511
-sysctl vm.overcommit_memory=1
+sysctl -a
+sysctl -w net.core.somaxconn=512
+sysctl -w vm.overcommit_memory=1
+sysctl -p
+sysctl -a
 redis-server --daemonize yes
