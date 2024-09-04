b21675860f7825c94d30b2ae7ddd28fd646b41d8
Bradley Erickson <bbwe24@gmail.com>
2024-09-04 10:42:14 -0400

    added prototype for new deck diff table

---
+++ src/components/deck_select.py
@@ -0,0 +1,174 @@
+import dash
+from dash import html, dcc, callback, Output, Input, State, MATCH, ALL, Patch, ctx, clientside_callback
+import dash_bootstrap_components as dbc
+import th_helpers.scraper.limitless
+import uuid
+
+import components.deck_label
+import utils.decklists
+
+LIMITLESSTCG = 'Limitless TCG'
+MANUAL_ENTRY = 'Manual'
+
+
+def create_deck_option(deck, icons, name):
+    opt = html.Div([
+        html.Span(components.deck_label.format_label({'name': deck, 'icons': icons})),
+        f' - {name}'
+    ], className='d-flex align-items-center')
+    return opt
+
+
+class DeckSelectAIO(html.Div):
+
+    class ids:
+        input_toggle = lambda aio_id: {
+            'component': 'DeckSelectAIO',
+            'subcomponent': 'input_toggle',
+            'aio_id': aio_id
+        }
+        decklist = lambda aio_id: {
+            'component': 'DeckSelectAIO',
+            'subcomponent': 'decklist_store',
+            'aio_id': aio_id
+        }
+        label = lambda aio_id: {
+            'component': 'DeckSelectAIO',
+            'subcomponent': 'label',
+            'aio_id': aio_id
+        }
+        limitless_wrapper = lambda aio_id: {
+            'component': 'DeckSelectAIO',
+            'subcomponent': 'limitless_wrapper',
+            'aio_id': aio_id
+        }
+        limitless_events = lambda aio_id: {
+            'component': 'DeckSelectAIO',
+            'subcomponent': 'limitless_events',
+            'aio_id': aio_id
+        }
+        limitless_players = lambda aio_id: {
+            'component': 'DeckSelectAIO',
+            'subcomponent': 'limitless_players',
+            'aio_id': aio_id
+        }
+        manual_wrapper = lambda aio_id: {
+            'component': 'DeckSelectAIO',
+            'subcomponent': 'manual_wrapper',
+            'aio_id': aio_id
+        }
+        manual_label = lambda aio_id: {
+            'component': 'DeckSelectAIO',
+            'subcomponent': 'manual_label',
+            'aio_id': aio_id
+        }
+        manual_input = lambda aio_id: {
+            'component': 'DeckSelectAIO',
+            'subcomponent': 'manual_input',
+            'aio_id': aio_id
+        }
+    
+    ids = ids
+
+    def __init__(
+        self,
+        event=None,
+        aio_id=None
+    ):
+        events = th_helpers.scraper.limitless.fetch_events()
+        if aio_id is None:
+            aio_id = str(uuid.uuid4())
+        component = dbc.Row([
+            dbc.Col(dbc.RadioItems(
+                options=[LIMITLESSTCG, MANUAL_ENTRY],
+                value=LIMITLESSTCG,
+                id=self.ids.input_toggle(aio_id)
+            ), md=2),
+            dbc.Col([
+                dbc.Input(value='', placeholder='Label', id=self.ids.manual_label(aio_id)),
+                dbc.Textarea(
+                    value='', placeholder='Paste decklist here', id=self.ids.manual_input(aio_id),
+                    size='sm', spellcheck='false'
+                )
+            ], md=10, id=self.ids.manual_wrapper(aio_id)),
+            dbc.Col([
+                dcc.Dropdown(id=self.ids.limitless_events(aio_id), options=[{
+                    'label': e[0], 'value': e[1]
+                } for e in events], value=event),
+                dcc.Dropdown(id=self.ids.limitless_players(aio_id))
+            ], md=10, id=self.ids.limitless_wrapper(aio_id), class_name='deck-diff-limitless-dropdowns'),
+            dcc.Store(id=self.ids.decklist(aio_id)),
+            html.Div(id=self.ids.label(aio_id), className='d-none'),
+            html.Hr()
+        ])
+        super().__init__(component)
+
+    clientside_callback(
+        f'''function (val) {{
+            if (val === '{LIMITLESSTCG}') {{ return ['d-none', 'deck-diff-limitless-dropdowns']; }}
+            return ['', 'd-none'];
+        }}
+        ''',
+        Output(ids.manual_wrapper(MATCH), 'class_name'),
+        Output(ids.limitless_wrapper(MATCH), 'class_name'),
+        Input(ids.input_toggle(MATCH), 'value')
+    )
+
+    @callback(
+        Output(ids.limitless_players(MATCH), 'options'),
+        Input(ids.limitless_events(MATCH), 'value')
+    )
+    def update_deck_options(tour):
+        if tour is None:
+            raise dash.exceptions.PreventUpdate
+        decks = th_helpers.scraper.limitless.fetch_decklists(tour)
+        options = [{
+            'label': create_deck_option(d[1], d[2], d[4]),
+            'value': f'{d[0]}:{d[3]}',
+            'search': f'{d[1]} {d[4]}'
+        } for d in decks]
+        return options
+
+    @callback(
+        Output(ids.decklist(MATCH), 'data'),
+        Input(ids.limitless_players(MATCH), 'value'),
+        Input(ids.manual_input(MATCH), 'value')
+    )
+    def update_selected_deck(deck, manual_input):
+        if ctx.triggered_id is None:
+            raise dash.exceptions.PreventUpdate
+        
+        subcomponent = ctx.triggered_id['subcomponent']
+
+        if subcomponent == 'limitless_players':    
+            base_url = 'https://limitlesstcg.com'
+            path = deck.split(':')[-1]
+            decklist = th_helpers.scraper.limitless.fetch_decklist(f'{base_url}{path}')
+            return decklist
+        elif subcomponent == 'manual_input':
+            decklist, _ = utils.decklists.parse_decklist(manual_input)
+            return decklist
+        raise dash.exceptions.PreventUpdate
+    
+    @callback(
+        Output(ids.label(MATCH), 'children'),
+        Input(ids.input_toggle(MATCH), 'value'),
+        Input(ids.limitless_events(MATCH), 'value'),
+        State(ids.limitless_events(MATCH), 'options'),
+        Input(ids.limitless_players(MATCH), 'value'),
+        State(ids.limitless_players(MATCH), 'options'),
+        Input(ids.manual_label(MATCH), 'value')
+    )
+    def update_label(toggle, event, events, player, players, manual_title):
+        if toggle == LIMITLESSTCG:
+            if event is None or player is None:
+                raise dash.exceptions.PreventUpdate
+            
+            label = [
+                next(e['label'] for e in events if e['value'] == event),
+                next(p['label'] for p in players if p['value'] == player),
+            ]
+            return label
+        elif toggle == MANUAL_ENTRY:
+            return manual_title
+        raise dash.exceptions.PreventUpdate
---
+++ src/pages/deck_diff_table.py
@@ -0,0 +1,117 @@
+import dash
+from dash import html, callback, Output, Input, State, Patch, ALL
+import dash_bootstrap_components as dbc
+
+from components import download_button, feedback_link, deck_select
+from utils import images, cards as _cards
+
+# TODO download button, prototype banner, more deck entry methods
+# caching limitless stuff
+
+page_title = 'Deck Diff Analyzer'
+page_icon = 'fa-code-compare'
+
+dash.register_page(
+    __name__,
+    path='/prototypes/deck-diff-new',
+    title=page_title,
+    image='tools.png',
+    icon=page_icon,
+    description='Compare and contrast multiple Pokémon TCG decklists with our Deck Diff tool. Generate intuitive Venn diagrams to visualize card overlaps and differences, helping you refine your deck choices and understand key strategic variations.'
+)
+
+prefix = 'deck-diff-table'
+deck_selection_table = f'{prefix}-deck-selection'
+add_row = f'{prefix}-add-row-btn'
+card_table = f'{prefix}-card-table'
+hide_common = f'{prefix}-hide-common'
+
+def layout():
+    cont = html.Div([
+        html.Div([
+            html.H2([html.I(className=f'fas {page_icon} me-1'), page_title], className='d-inline-block'),
+            download_button.DownloadImageAIO(dom_id=card_table, className='float-end')
+        ]),
+        dbc.Alert(html.Ul([
+            html.Li([html.Strong('Prototype dashboard:'), ' This dashboard is a work in progress. Some things are not yet finalized.']),
+            html.Li([html.Strong('Purpose:'), ' Analyze and compare multiple decklists.']),
+            html.Li([html.Strong('Usage:'), ' Select decks from LimitlessTCG or input decks manually to compare.']),
+            feedback_link.list_item,
+        ], className='mb-0'), id='tour-meta-report-info-alert', color='info'),
+        html.Div([deck_select.DeckSelectAIO(aio_id=0)], id=deck_selection_table),
+        dbc.Button([html.I(className='fas fa-plus me-1'), 'Add'], id=add_row),
+        dbc.Checkbox(id=hide_common, value=False, label='Hide common cards'),
+        dbc.Spinner(dbc.Table(id=card_table, bordered=True))
+    ])
+    return cont
+
+
+@callback(
+    Output(deck_selection_table, 'children'),
+    Input(add_row, 'n_clicks'),
+    State(deck_select.DeckSelectAIO.ids.limitless_events(ALL), 'value')
+)
+def add_row_to_table(clicks, curr_events):
+    if not clicks:
+        raise dash.exceptions.PreventUpdate
+    patch = Patch()
+    last_event = curr_events[-1] if len(curr_events) > 0 else None
+    patch.append(deck_select.DeckSelectAIO(aio_id=clicks, event=last_event))
+    return patch
+
+
+@callback(
+    Output(card_table, 'children'),
+    Input(deck_select.DeckSelectAIO.ids.decklist(ALL), 'data'),
+    Input(deck_select.DeckSelectAIO.ids.label(ALL), 'children'),
+    Input(hide_common, 'value')
+)
+def update_card_table(decks, labels, hide):
+    cards = {}
+    for i, deck in enumerate(decks):
+        if deck is None: continue
+        for card in deck:
+            id = f'{card["set"]}-{card["number"]}'
+            if id not in cards:
+                cards[id] = card
+                cards[id]['decks'] = []
+            cards[id]['decks'].append((i, card['count']))
+    for card in cards:
+        counts = [c[1] for c in cards[card]['decks']]
+        cards[card]['count'] = sum(counts) / len(decks)
+    card_info = []
+    for card in cards.values():
+        deck_counts = card['decks']
+        fetched_card = _cards.get_card(card)
+        fetched_card['decks'] = deck_counts
+        card_info.append(fetched_card)
+    sorted_cards = _cards.sort_deck(card_info)
+
+    headers = [html.Th('')]
+    for label in labels:
+        headers.append(html.Th(html.Div(html.Div(label), className='rotate')))
+    rows = []
+    for card in sorted_cards:
+        card_id = f"{card['set']}-{card['number'].zfill(3)}"
+        cells = [html.Td([
+            dbc.Popover(
+                dbc.PopoverBody(
+                    html.Img(src=images.get_card_image(card_id, 'SM'), className='w-100')
+                ), target=card_id, trigger='hover legacy', placement='bottom'
+            ),
+            card['name']
+        ], id=card_id)]
+
+        for _ in decks:
+            cells.append(html.Td(''))
+        for item in card['decks']:
+            color = 'success' if item[1] > card['count'] else 'danger' if item[1] < card['count'] else ''
+            diff = abs(item[1] - card['count'])
+            opacity = '25' if diff >= 2 else '50' if diff >= 1 else '75'
+            cells[item[0]+1] = html.Td(item[1], className=f'bg-opacity-{opacity} bg-{color} text-center')
+
+        if hide and len(card['decks']) == len(decks) and all(c[1] == card['decks'][0][1] for c in card['decks']):
+            continue
+        rows.append(html.Tr(cells))
+    table_children = [html.Thead(headers, className='sticky-top')] + [html.Tbody(rows)]
+    return table_children
