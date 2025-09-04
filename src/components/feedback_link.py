65a00c0d450698b09f9cded4e9a6babf5f359c89
Bradley Erickson <bbwe24@gmail.com>
2025-09-04 07:38:16 -0400

    added missing feedback link

--- src/components/feedback_link.py
+++ src/components/feedback_link.py
@@ -1,6 +1,6 @@
 from dash import html, dcc
 
-link_item = dcc.Link('Feedback Form', href='/feedback')
+link_item = lambda page: dcc.Link('Feedback Form', href=f'/feedback?page={page}')
 
 list_item = lambda page: html.Li([
     html.Strong('Need Assistance:'), ' If you encounter an issue or have suggestions, please submit a ',
